d78c86802b09fa38839243405dd5c25e7c2a342d
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-04 14:42:51 -0300

    Construir Atributos e métodos

--- src/main/java/br/com/zup/investimento/DTO/RetornoDTO.java
+++ src/main/java/br/com/zup/investimento/DTO/RetornoDTO.java
@@ -1,4 +1,41 @@
 package br.com.zup.investimento.DTO;
 
 public class RetornoDTO {
+    private double valorInicial;
+    private double valorLucro;
+    private double valorTotal;
+
+    public RetornoDTO() {
+
+    }
+
+    public RetornoDTO(double valorInicial, double valorLucro, double valorTotal) {
+        this.valorInicial = valorInicial;
+        this.valorLucro = valorLucro;
+        this.valorTotal = valorTotal;
+    }
+
+    public double getValorInicial() {
+        return valorInicial;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+
+    public double getValorLucro() {
+        return valorLucro;
+    }
+
+    public void setValorLucro(double valorLucro) {
+        this.valorLucro = valorLucro;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
