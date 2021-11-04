85e82a8bc47cb7858ceb6385f567f1db2f4a8f9b
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-04 13:41:38 -0300

    Criar Classe Enum Risco

--- src/main/java/br/com/zup/investimento/Risco.java
+++ src/main/java/br/com/zup/investimento/Risco.java
@@ -1,4 +1,23 @@
 package br.com.zup.investimento;
 
 public enum Risco {
+    ALTO(0.025), MEDIO(0.015), BAIXO(0.005);
+
+    public double valorRisco;
+
+    Risco() {
+    }
+
+    Risco(double valorRisco) {
+        this.valorRisco = valorRisco;
+    }
+
+    public double getValorRisco() {
+        return valorRisco;
+    }
+
+    public void setValorRisco(double valorRisco) {
+        this.valorRisco = valorRisco;
+    }
 }
+
