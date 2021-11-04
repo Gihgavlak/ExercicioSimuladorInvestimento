b19c805897fb3764e0c9753bbaf509dd026fd380
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-04 15:38:06 -0300

    Adicionar commits das classes feitas-> ainda não finalizada

--- src/main/java/br/com/zup/investimento/Controller/InvestimentoController.java
+++ src/main/java/br/com/zup/investimento/Controller/InvestimentoController.java
@@ -1,2 +1,14 @@
-package br.com.zup.investimento.Controller;public class InvestimentoController {
+package br.com.zup.investimento.Controller;
+
+import br.com.zup.investimento.Service.InvestimentoService;
+import org.springframework.web.bind.annotation.RequestMapping;;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/investimento")
+public class InvestimentoController {
+
+    private InvestimentoService investimentoService;
+
+
 }
--- src/main/java/br/com/zup/investimento/Service/InvestimentoService.java
+++ src/main/java/br/com/zup/investimento/Service/InvestimentoService.java
@@ -1,7 +1,12 @@
 package br.com.zup.investimento.Service;
 
+import br.com.zup.investimento.DTO.RetornoDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InvestimentoService {
+
+    private RetornoDTO retornoInvestimento = new RetornoDTO();
+
+
 }
